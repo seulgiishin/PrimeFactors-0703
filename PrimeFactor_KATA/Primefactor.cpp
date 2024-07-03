c8d99e6ca63c83da54f5d2302624225f837b7dc1
seulgiishin <tls940807@gmail.com>
2024-07-03 10:57:12 +0900

    [feature] page 34

--- PrimeFactor_KATA/Primefactor.cpp
+++ PrimeFactor_KATA/Primefactor.cpp
@@ -1,3 +1,13 @@
+#include <vector>
+using namespace std;
 class Primefactor {
+public:
+	vector<int> of(int number) {
+		vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
+		return result;
+	}
 
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,9 +1,18 @@
 #include "pch.h"
 #include "../PrimeFactor_KATA/Primefactor.cpp"
 
+#include <vector>
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(PrimeFactors, Of1) {
+	Primefactor pf;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, pf.of(1));
+}
+
+
+TEST(PrimeFactors, Of2) {
+	Primefactor pf;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, pf.of(2));
 }
