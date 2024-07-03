582b10b70a02d06d595598f90b3ff0b3df8a5333
seulgiishin <tls940807@gmail.com>
2024-07-03 11:03:45 +0900

    [feature] page 41

--- PrimeFactor_KATA/Primefactor.cpp
+++ PrimeFactor_KATA/Primefactor.cpp
@@ -5,7 +5,13 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
-			result.push_back(number);
+			if (number == 4) {
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else {
+				result.push_back(number);
+			}
 		}
 		return result;
 	}
